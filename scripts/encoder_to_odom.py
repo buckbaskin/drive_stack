04fd923d4d136b88524a34bde1453aceaa43ea3a
Buck Baskin <mobile.wbaskin@gmail.com>
2015-10-19 00:17:22 -0400

    Encoder -> Odom passes pylint (import errors from snowmower_msgs.msg)

--- scripts/encoder_to_odom.py
+++ scripts/encoder_to_odom.py
@@ -1,15 +1,51 @@
+"""
+ENCODER TO ODOM
+
+ROS Node for converting encoder ticks to a wheel odometry measurement of
+position.
+"""
+
 import rospy
+from tf import transformations as tft
 import math
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Quaternion
 from snowmower_msgs.msg import EncMsg
 
 def heading_to_quaternion(heading):
-    # TODO(buckbaskin): use transformations
-    q = Quaternion()
-    return q
+    """
+    Converts a Euler yaw/heading angle to equivalent quaternion
+    input: euler heading in radians
+    output: nav_msgs.msg.Quaternion
+    """
+
+    quat = tft.quaternion_from_euler(0, 0, heading)
+
+    quaternion = Quaternion()
+    quaternion.x = quat[0]
+    quaternion.y = quat[1]
+    quaternion.z = quat[2]
+    quaternion.w = quat[3]
+    return quaternion
 
 class WheelOdometryGenerator(object):
+    """
+    WheelOdometryGenerator: ROS Node that implements encoder ticks to position
+
+    wog = WheelOdometryGenerator()
+    wog.start_node()
+    """
+
+    # pylint: disable=too-many-instance-attributes
+    # These attributes represent state, last data, and pub/sub
+
+    # pylint: disable=invalid-name
+    # The variables for state are valid as x, y, v (linear velocity)
+
+    track_width = 1
+    wheel_diam = .333
+    encoders_per_rev = 100
+
     def __init__(self):
         self.x = 0
         self.y = 0
@@ -21,11 +57,17 @@ class WheelOdometryGenerator(object):
 
         self.seq = 1
 
-        self.track_width = 1
-        self.wheel_diam = .333
-        self.encoders_per_rev = 100
+        self.sub = None
+        self.pub = None
 
     def process_encoder_msg(self, msg):
+        """
+        process_encoder_msg: callback to take in new data, and update state
+
+        input: snowmower_msgs.msg.EncMsg
+        output: None
+        """
+
         if self.last_enc is None:
             self.last_enc = msg
         dleft = msg.left - self.last_enc.left
@@ -43,10 +85,12 @@ class WheelOdometryGenerator(object):
             arc_right = self.distance(dright)
 
             if arc_left < arc_right:
-                r = (self.track_width/2)*((arc_right+arc_left)/(arc_right-arc_left))
+                r = ((self.track_width/2)*
+                    ((arc_right+arc_left)/(arc_right-arc_left)))
                 theta = arc_left/(r-(self.track_width/2))
             else: # arc_left > arc_right:
-                r = (self.track_width/2)*((arc_right+arc_left)/(arc_left-arc_right))
+                r = ((self.track_width/2)*
+                    ((arc_right+arc_left)/(arc_left-arc_right)))
                 theta = arc_left/(r+(self.track_width/2))
             v = ((dleft+dright)/2)/dt
             omega = theta/dt
@@ -58,6 +102,13 @@ class WheelOdometryGenerator(object):
         self.last_enc = msg
 
     def send_current_odom(self):
+        """
+        send_current_odom: publish the current state as an Odometry message
+
+        input: None
+        output: (ROS publish) nav_msgs.msg.Odometry
+        """
+
         o = Odometry()
         o.header.seq = self.seq
         o.header.stamp = rospy.Time.now()
@@ -76,29 +127,52 @@ class WheelOdometryGenerator(object):
         self.pub.publish(o)
 
     def start_node(self):
+        """
+        start_node: Run the ROS node by intializing the publisher and subscriber
+        """
         self.pub = rospy.Publisher('/odom', Odometry, queue_size=1)
         self.sub = rospy.Subscriber('/enc', EncMsg, self.process_encoder_msg)
 
     # GEOMETRY
 
     def distance(self, ticks):
-        return ticks*1.0/self.encoders_per_rev*math.PI*self.wheel_diam
+        """
+        distance: calculate the distance traveled per given ticks based on given
+            robot geometry
+        """
+        return ticks*1.0/self.encoders_per_rev*math.pi*self.wheel_diam
 
     def update_state(self, avg_v, avg_omega, dt):
+        """
+        update_state: move the state forward one step
+
+        state has:
+        initial x, y, heading, linear velocity, angular velocity
+
+        input:
+        average linear velocity, average angular velocity and change in time
+
+        output:
+        None
+        (modifies instance state based on calculated final x, y,
+            linear velocity, angular velocity)
+        """
         # input is observed average velocity (distance / time), angular velocity
         # calculates the linear value from start v (current state) to end v (new
         # state) based on this average.
 
-        dr = avg_v*dt
-        dtheta = avg_omega*dt
+        dr = avg_v*dt # distance traveled forward
+        dtheta = avg_omega*dt # angle swept
 
         average_heading = self.heading + avg_omega/2.0
 
-        dy = dr * math.sin(average_heading)
+        dy = dr * math.sin(average_heading) # distance traveled in x, y
         dx = dr * math.cos(average_heading)
-        end_v = avg_v + (avg_v - self.v)
+
+        end_v = avg_v + (avg_v - self.v) # final linear, angular velocity
         end_omega = avg_omega + (avg_omega - self.omega)
 
+        # assign new values to update state
         self.x += dx
         self.y += dy
         self.heading += dtheta
