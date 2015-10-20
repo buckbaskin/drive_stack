8fae0832c47ca078ded401ac46eebf4fd1902065
Buck Baskin <mobile.wbaskin@gmail.com>
2015-10-20 00:48:56 -0400

    WIP | pylint over driver.py, minus doc strings

--- scripts/driver.py
+++ scripts/driver.py
@@ -6,9 +6,9 @@ Copyright 2015 William Baskin
 /*****************************************
  LICENSE SUMMARY
 
- This package is licensed under the 
+ This package is licensed under the
     MIT License. Please see the LICENSE.md
-    file in the root folder for the 
+    file in the root folder for the
     complete license.
 
  *****************************************/
@@ -25,9 +25,13 @@ Copyright 2015 William Baskin
  '''
 
 import rospy
+import math
 from tf import transformations as tft
-from geometry_msgs.msg import Odometry, Twist
-from drive_stack.srv import Goal
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist, Quaternion
+
+# tentatively removed, might need it to call services properly
+# from drive_stack.srv import Goal
 
 def quaternion_to_heading(quaternion):
     """
@@ -36,7 +40,7 @@ def quaternion_to_heading(quaternion):
     output: euler heading in radians
     """
     quat = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
-    yaw = tft.euler_from_quaternion(quaternion)[2]
+    yaw = tft.euler_from_quaternion(quat)[2]
     return yaw
 
 def heading_to_quaternion(heading):
@@ -55,27 +59,27 @@ def heading_to_quaternion(heading):
     quaternion.w = quat[3]
     return quaternion
 
-def dot_product(a, b):
+def dot_product(vec1, vec2):
     """
     calcuates the dot product of two tuples/vectors a, b
-    input: 2 three-tuples a, b
-    output: double: dot product 
+    input: 2 three-tuples a, vec2
+    output: double: dot product
     """
-    return a[0]*b[0]+a[1]*b[1]+a[2]*b[2]
+    return vec1[0]*vec2[0]+vec1[1]*vec2[1]+vec1[2]*vec2[2]
 
-def cross_product(a, b):
+def cross_product(vec1, vec2):
     """
     calcuates the cross product of two tuples/vectors a, b
-    input: 2 three-tuples a, b
-    output: three-tuple (cross product of a, b)
+    input: 2 three-tuples vec1, vec2
+    output: three-tuple (cross product of a, vec2)
 
      i    j    k
     a[0] a[1] a[2]
     b[0] b[1] b[2]
     """
-    i = a[1]*b[2]-a[2]*b[1]
-    j = a[0]*b[2]-a[2]*b[0]
-    k = a[0]*b[1]-a[1]*b[0]
+    i = vec1[1]*vec2[2]-vec1[2]*vec2[1]
+    j = vec1[0]*vec2[2]-vec1[2]*vec2[0]
+    k = vec1[0]*vec2[1]-vec1[1]*vec2[0]
     return (i, j, k,)
 
 def scale(vector, magnitude):
@@ -90,15 +94,32 @@ def unit(vector):
     """
     returns the unit vector in the same direction as the given vector
     """
-    length = sqrt(vector[0]*vector[0]+vector[1]*vector[1]+vector[2]*vector[2])
+    length = math.sqrt(vector[0]*vector[0]+vector[1]*vector[1]+
+        vector[2]*vector[2])
     return scale(vector, 1.0/length)
 
 class Driver(object):
     """
     See above
     """
+    # pylint: disable=invalid-name
+    # constants are somewhat standard, will be changed based on new math
+    # see (Kanayama, Fahroo 1997 "A New Line Tracking Method...")
+    a = 1
+    b = 1
+    c = 1
+    j = 1
+    k = 1
+
     def __init__(self):
-        pass
+        # ROS pub/sub
+        self.cmd_vel = None
+        self.position = None
+        # ROS services
+        self.lead_back = None
+        self.lead_start = None
+        self.lead_next = None
+        self.lead_goal = None
 
     def wait_for_services(self):
         """
@@ -134,23 +155,24 @@ class Driver(object):
         """
         Takes in a new odom/best position estimate
         sends out a new control command based on this best estimate of location
-        Note: this may not work effectively for control. It is an example 
+        Note: this may not work effectively for control. It is an example
         implementation at best.
         """
-        next = self.lead_goal()
-        if (dist(next, odom) < .04):
+        next_goal = self.lead_goal()
+        if self.dist(next_goal, odom) < .04:
             # if you are .04 m or less from the goal, move forward
             # NOTE: this might change.
-            # for example, you may want to look at the next two points ahead
-            #  and see if you want to skip the current one, or if you are ahead
-            #  of the current one in the direction that you want to go
-            next = self.lead_next()
+            # for example, you may want to look at the next_goal two points
+            #  ahead and see if you want to skip the current one, or if you are
+            #  ahead of the current one in the direction that you want to go
+            next_goal = self.lead_next()
 
         # errors along axis "x", off axis "y", heading "theta"
-        along, off, heading = calc_errors(odom, next)
+        along, off, heading = self.calc_errors(odom, next_goal)
 
-        v_error = odom.twist.twist.linear.x - next.twist.twist.linear.x
-        omega_error = odom.twist.twist.angular.z - next.twist.twist.angular.z
+        v_error = odom.twist.twist.linear.x - next_goal.twist.twist.linear.x
+        omega_error = (odom.twist.twist.angular.z -
+            next_goal.twist.twist.angular.z)
 
         linear_vel = odom.twist.twist.linear.x
         angular_vel = odom.twist.twist.angular.z
@@ -164,28 +186,21 @@ class Driver(object):
         # accel desired (correction) = -bv - kx
 
         # for now, just a linear weighted correction
-        b = 1
-        k = 1
-
-        linear_vel += -k*along
-        linear_vel += -b*v_error
+        linear_vel += -self.k*along
+        linear_vel += -self.b*v_error
 
         # angular velocity needs to adjust for heading error
         # as well as offset from the desired axis
 
         # F = -j(theta) - a(dtheta/dt)
-        j = 1
-        a = 1
-
-        angular_vel += -j*heading_error
-        angular_vel += -a*omega_error
+        angular_vel += -self.j*heading
+        angular_vel += -self.a*omega_error
 
         # correction for offset from axis
         # positive error is to the left, so correction is to the right/negative
         # linear-like assumption
         # correction = -c*off , where off is the error off axis
-        c = 1
-        angular_vel += -c*off
+        angular_vel += -self.c*off
 
         twist_out = Twist()
         twist_out.linear.x = linear_vel
@@ -193,21 +208,24 @@ class Driver(object):
         self.cmd_vel.publish(twist_out)
 
     def calc_errors(self, location, goal):
-        return (self.along_axis_error(location, goal), 
-            self.off_axis_error(location, goal), 
-            self.heading_error(location, goal),)
+        along = self.along_axis_error(location, goal)
+        off = self.off_axis_error(location, goal)
+        heading = self.heading_error(location, goal)
+        return (along, off, heading,)
 
+    # pylint: disable=no-self-use
+    # these are clearly used. See ^ calc_errors above
     def along_axis_error(self, location, goal):
-        relative_position_x = (location.pose.pose.position.x - 
+        relative_position_x = (location.pose.pose.position.x -
             goal.pose.pose.position.x)
-        relative_position_y = (location.pose.pose.position.y - 
+        relative_position_y = (location.pose.pose.position.y -
             goal.pose.pose.position.y)
-        relative_position_z = (location.pose.pose.position.z - 
+        relative_position_z = (location.pose.pose.position.z -
             goal.pose.pose.position.z)
 
         # relative position of the best estimate position and the goal
         # vector points from the goal to the location
-        relative_position = (relative_position_x, relative_position_y, 
+        relative_position = (relative_position_x, relative_position_y,
             relative_position_z)
 
         goal_heading = quaternion_to_heading(goal.pose.pose.position)
@@ -221,25 +239,24 @@ class Driver(object):
         return dot_product(relative_position, goal_vector)
 
     def off_axis_error(self, location, goal):
-        relative_position_x = (location.pose.pose.position.x - 
+        relative_position_x = (location.pose.pose.position.x -
             goal.pose.pose.position.x)
-        relative_position_y = (location.pose.pose.position.y - 
+        relative_position_y = (location.pose.pose.position.y -
             goal.pose.pose.position.y)
-        relative_position_z = (location.pose.pose.position.z - 
+        relative_position_z = (location.pose.pose.position.z -
             goal.pose.pose.position.z)
 
         # relative position of the best estimate position and the goal
         # vector points from the goal to the location
-        relative_position = (relative_position_x, relative_position_y, 
+        relative_position = (relative_position_x, relative_position_y,
             relative_position_z)
 
         goal_heading = quaternion_to_heading(goal.pose.pose.position)
         goal_vector_x = math.cos(goal_heading)
         goal_vector_y = math.sin(goal_heading)
-        goal_vector_z = 0.0
 
         # vector in the direction of the goal heading, axis of desired motion
-        goal_vector = (goal_vector_x, goal_vector_y, goal_vector_z)
+        goal_vector = (goal_vector_x, goal_vector_y, 0.0)
 
         relative_along_goal = scale(unit(relative_position),
             dot_product(relative_position, goal_vector))
@@ -248,7 +265,7 @@ class Driver(object):
         relative_normal_y = relative_position[1]-relative_along_goal[1]
         relative_normal_z = relative_position[2]-relative_along_goal[2]
 
-        return sqrt(relative_normal_x*relative_normal_x+
+        return math.sqrt(relative_normal_x*relative_normal_x+
             relative_normal_y*relative_normal_y+
             relative_normal_z*relative_normal_z)
 
@@ -257,19 +274,30 @@ class Driver(object):
         """
         return difference in heading between location and goal
         """
-        loc_head =quaternion_to_heading(location.pose.pose.quaternion)
-        goal_head =quaternion_to_heading(goal.pose.pose.quaternion)
+        loc_head = quaternion_to_heading(location.pose.pose.quaternion)
+        goal_head = quaternion_to_heading(goal.pose.pose.quaternion)
         return loc_head - goal_head
+
+    def dist(self, odom1, odom2):
+        """
+        returns linear distance between two odometry messages
+        """
+        # pylint: disable=invalid-name
+        # x and y accurately represent the axis that I'm referring to
+        x = odom1.pose.pose.position.x - odom2.pose.pose.position.x
+        y = odom1.pose.pose.position.y - odom2.pose.pose.position.y
+        return math.sqrt(x*x+y*y)
 
     def run_node(self):
         self.wait_for_services()
         self.init_node()
-        
-        rt = rospy.rate(10)
+
+        rt = rospy.Rate(10)
         while not rospy.is_shutdown():
-            self.cmd_vel.publish(next_cmd())
             rt.sleep()
 
 if __name__ == '__main__':
+    # pylint: disable=invalid-name
+    # ignoring the trivial naming of the Driver class for startup and run
     d = Driver()
     d.run_node()
