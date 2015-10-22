20458a2b425ba9031ce07960814ecf80b3da4aeb
Buck Baskin <mobile.wbaskin@gmail.com>
2015-10-22 19:43:08 -0400

    Fixed issues. Closes #26 for real this time

--- scripts/vel_profile_execution.py
+++ scripts/vel_profile_execution.py
@@ -10,41 +10,42 @@ It's probably advised to only run one of the main_ methods at the bottom.
 
 import rospy
 import math
-import rospy.loginfo as log
-from geometery_msgs.msg import Twist
+from geometry_msgs.msg import Twist
 
 def accel(start_v, end_v, start_w, end_w, time, pub):
-    steps = int(floor(time/.05))
+    steps = int(math.floor(time/.05))
     dv = (end_v - start_v)/(1.0*steps)
     dw = (end_w - start_w)/(1.0*steps)
     rate = rospy.Rate(1.0/.05)
     twist = Twist()
-    log('accelerating...')
+    rospy.loginfo('accelerating...')
     for i in range(0, steps):
         twist.linear.x = (start_v + dv*steps)
         twist.angular.z = (start_w + dw*steps)
         pub.publish(twist)
-    log('...done accelerating')
+        rate.sleep()
+    rospy.loginfo('...done accelerating')
 
 def constant(start_v, start_w, time, pub):
-    steps = int(floor(time/.05))
+    steps = int(math.floor(time/.05))
     rate = rospy.Rate(1.0/.05)
     twist = Twist()
-    log('constant velocity')
+    rospy.loginfo('constant velocity')
     for i in range(0, steps):
         twist.linear.x = start_v
         twist.angular.z = start_w
         pub.publish(twist)
-    log('...done constant velocity')
+        rate.sleep()
+    rospy.loginfo('...done constant velocity')
 
 def main_straight():
     rospy.init_node('velocity_profile')
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
     slow_start = rospy.Rate(1)
-    for i in range(5, 1, -1):
-        log(str(i)+'...')
+    for i in range(5, 0, -1):
+        rospy.loginfo(str(i)+'...')
         slow_start.sleep()
-    log('Go')
+    rospy.loginfo('Go')
     # 18ft = 5.4864m
     # constant dist = 4.9864
 
@@ -58,18 +59,20 @@ def main_straight():
     # total distance = 5.4864m (18ft)
 
     # stop
+    rospy.loginfo('done with main_straight')
     pub.publish(Twist())
     slow_start.sleep()
     pub.publish(Twist())
+    rospy.loginfo('done with main_straight')
 
 def main_turn_in_place():
     rospy.init_node('velocity_profile')
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
     slow_start = rospy.Rate(1)
-    for i in range(5, 1, -1):
-        log(str(i)+'...')
+    for i in range(5, 0, -1):
+        rospy.loginfo(str(i)+'...')
         slow_start.sleep()
-    log('Go')
+    rospy.loginfo('Go')
     # 360deg = 2*pi rad
     # constant dist = 2*pi - .30 rad
 
@@ -83,18 +86,20 @@ def main_turn_in_place():
     # total distance = 2*pi rad (360 deg)
 
     # stop
+    rospy.loginfo('done with main_turn_in_place')
     pub.publish(Twist())
     slow_start.sleep()
     pub.publish(Twist())
+    rospy.loginfo('done with main_turn_in_place')
 
 def main_circle(radius):
     rospy.init_node('velocity_profile')
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
     slow_start = rospy.Rate(1)
-    for i in range(5, 1, -1):
-        log(str(i)+'...')
+    for i in range(5, 0, -1):
+        rospy.loginfo(str(i)+'...')
         slow_start.sleep()
-    log('Go')
+    rospy.loginfo('Go')
     # v = wr
     v_max = .5
     w_max = v_max/radius
@@ -103,28 +108,26 @@ def main_circle(radius):
         v_max = w_max*radius
 
     dist = 2.0*radius*math.pi
+    rospy.loginfo('r: '+str(radius)+' v: '+str(v_max)+' w: '+str(w_max))
     dist_const = dist - v_max
     time_const = dist_const / v_max
 
-    # dist = 1 sec * 0.30 rad/s / 2 = .15 rad
     accel(0.0,v_max, 0.0,w_max, 1.0,pub)
-    # dist = 19.94395 sec * 0.3 rad/s = 2*pi - .30 rad
     constant(v_max,w_max, time_const, pub)
-    # dist = 1 sec * 0.30 rad/s / 2 = .15 rad
     accel(v_max,0.0, w_max,0.0, 1.0,pub)
 
-    # total distance = 2*pi rad (360 deg)
-
     # stop
+    rospy.loginfo('done with main_circle')
     pub.publish(Twist())
     slow_start.sleep()
     pub.publish(Twist())
+    rospy.loginfo('done with main_circle')
 
 
 
 
 if __name__ == '__main__':
-    main_straight() # runs forward 18ft
+    # main_straight() # runs forward 18ft
     # main_turn_in_place() # turns in place 360 degrees
-    # radius = 2.7432 # meters
-    # main_circle(radius) # drives one circle at the given radius
+    radius = 2.7432 # meters, 9ft 
+    main_circle(radius) # drives one circle at the given radius
