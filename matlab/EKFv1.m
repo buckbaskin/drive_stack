eb5b1040d40dd405e441b368c3507bf4d468b060
William Baskin <mobile.wbaskin@gmail.com>
2015-11-05 00:23:48 -0500

    Initial matlab ekf framework

---
+++ matlab/EKFv1.m
@@ -0,0 +1,15 @@
+classdef EKFv1
+    %EKFv1 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+        function [pose, covar, likelihood] = update(old_pose, old_covar, control, features, corresp, map)
+            % TODO(buckbaskin) start here
+        end
+    end
+    
+end
+
