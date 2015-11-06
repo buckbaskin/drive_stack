daab61366ef05e30ffe91fdeabc2918215dc8bf8
Buck Baskin <mobile.wbaskin@gmail.com>
2015-11-06 17:15:00 -0500

    Working on updating TravisCI to work with ROS

---
+++ install_dependencies.sh
@@ -0,0 +1,7 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-get update
+sudo apt-get install ros-indigo-ros-base
+sudo rosdep init
+sudo rosdep update
+source /opt/ros/indigo/setup.bash
