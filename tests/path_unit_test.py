dbe8cc6cce976bf59f9f327874546ef97dbedca0
Buck Baskin <mobile.wbaskin@gmail.com>
2015-11-06 18:02:46 -0500

    Iterating TravisCI #06

--- tests/path_unit_test.py
+++ tests/path_unit_test.py
@@ -1,21 +1,27 @@
+#!/usr/bin/env python
 PKG = 'drive_stack'
 NAME = 'test_unit_path'
 
 import sys
 import unittest
-from drive_stack.scripts.path import Path
+print sys.path
+sys.path.append('/home/buck/ros_workspace/src/drive_stack/scripts')
+# from drive_stack.scripts import Path
+import path
 
 class TestPathUnit(unittest.TestCase):
 
-    def __init__(self):
-        self.p = Path()
+    def __init__(self, *args):
+        super(TestPathUnit, self).__init__('test_example')
+        self.p = path.Path()
 
     def test_example(self):
         self.assertEquals(1, 1, "1 != 1")
 
 def run_tests():
-	import rosunit
+    import rosunit
     rosunit.unitrun(PKG, NAME, TestPathUnit)
+
 
 if __name__ == '__main__':
     run_tests()
--- tests/test_all_of_the_units.py
+++ tests/test_all_of_the_units.py
@@ -1,7 +1,8 @@
-from tests import path_unit_test
+#!/usr/bin/env python
+import path_unit_test
 
 path_unit_test.run_tests()
 
-from tests import path_integration_test
+import path_integration_test
 
 path_integration_test.run_tests()
