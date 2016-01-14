3719d5db20428628cb8dd39d5f188d8f9aab8f2f
Buck Baskin <mobile.wbaskin@gmail.com>
2016-01-14 12:49:24 -0500

    Adding noise for most dimensions if there is non-zero data for them

--- scripts/odom_noisy.py
+++ scripts/odom_noisy.py
@@ -13,13 +13,14 @@ class NoisyOdom(object):
         self.odom = rospy.Subscriber('/base_pose_ground_truth', Odometry, self.process_position)
         self.noisy_odom = rospy.Publisher('/ekf_simulated', Odometry, queue_size=1)
         self.original_odom = rospy.Publisher('/ekf_clean', Odometry, queue_size=1)
+        rospy.loginfo('noisy odom running.')
         rospy.spin()
 
     def process_position(self, odom):
         odom.header.frame_id = 'map'
         self.original_odom.publish(odom)
 
-        print('add noise')
+        # print('add noise')
 
         # noise = np.random.normal(0,1)
 
@@ -32,12 +33,26 @@ class NoisyOdom(object):
         odom.pose.pose.position.x += noise(0,1) # 1
         odom.pose.pose.position.y += noise(0,1) # 2
 
+        if odom.pose.pose.position.z: # if it is not 0
+            odom.pose.pose.position.z +=  noise(0,1)
+
         heading = quaternion_to_heading(odom.pose.pose.orientation)
         heading += noise(0,1)
         odom.pose.pose.orientation = heading_to_quaternion(heading) # 3
 
         odom.twist.twist.linear.x += noise(0,1) # 4
+
+        if odom.twist.twist.linear.y: # if it is not 0
+            odom.twist.twist.linear.y +=  noise(0,1)
+        if odom.twist.twist.linear.z: # if it is not 0
+            odom.twist.twist.linear.z +=  noise(0,1)
+
         odom.twist.twist.angular.z += noise(0,1) # 5
+
+        if odom.twist.twist.angular.x: # if it is not 0
+            odom.twist.twist.angular.x +=  noise(0,1)
+        if odom.twist.twist.angular.y: # if it is not 0
+            odom.twist.twist.angular.y +=  noise(0,1)
 
         self.noisy_odom.publish(odom)
 
